e419285e10fe3f0fdbf617c7763c8ceaa64841c2
Pete Stewart <petestew@gmail.com>
2020-09-11 11:40:36 -0500

    search bar repopulates if empty on blur

--- src/UI/SearchBar/SearchBar.js
+++ src/UI/SearchBar/SearchBar.js
@@ -17,6 +17,12 @@ const SearchBar = (props) => {
     props.click(value);
   };
 
+  const resetInput = () => {
+    if (value === '') {
+      setValue(props.placeholder);
+    }
+  };
+
   return (
     <div className="SearchBar">
       <form className="form-inline my-2 my-lg-0 filled">
@@ -24,12 +30,13 @@ const SearchBar = (props) => {
           className="form-control mr-sm-2"
           id="location-input"
           type="search"
-          // placeholder={props.placeholder}
+          placeholder={value || 'Enter address, city or zipcode'}
           value={value}
           onClick={() => setValue('')}
+          onBlur={resetInput}
           onChange={inputHandler}
           aria-label="Search" />
-        <button className="btn btn-outline-dark my-2 my-sm-0" type="submit" onClick={submitHandler}><i className="fas fa-search"></i></button>
+        <button className="btn btn-outline-dark my-2 my-sm-0 search-button" type="submit" onClick={submitHandler}><i className="fas fa-search"></i></button>
       </form>
     </div>
   );
