dd2fbf221ca426388d438ebf85f5513e0f16966d
Pete Stewart <petestew@gmail.com>
2020-09-15 09:49:02 -0500

    cleaned up RestaurantUrl component

--- src/components/RestaurantUrl/RestaurantUrl.js
+++ src/components/RestaurantUrl/RestaurantUrl.js
@@ -3,16 +3,14 @@ import React from 'react';
 
 import './RestaurantUrl.scss';
 
-const RestaurantUrl = (props) => {
-  return (
+const RestaurantUrl = (props) => (
     <div className="RestaurantUrl">
     {props.url
       ? <a href={props.url} target="_blank" rel="noopener noreferrer">{props.children}</a>
       : <span className="link-disabled">{props.children}</span>
     }
     </div>
-  );
-};
+);
 
 // RestaurantUrl.propTypes = {}
 
